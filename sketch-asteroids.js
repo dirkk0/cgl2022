d0315c4ecbcefaea2b7a2d6ff3e47a62bb48c787
Dirk Krause <dirkk0@googlemail.com>
2023-01-11 18:28:10 +0100

    Update sketch-asteroids.js

--- sketch-asteroids.js
+++ sketch-asteroids.js
@@ -1,29 +1,18 @@
 const s = (p5) => {
-    let movie
+
+    let posX = 100
+    let posY = 100
+
     p5.setup = function() {
         p5.createCanvas(400, 400);
-        movie = p5.createVideo("test.mov");
-        movie.hide()
     }
     p5.draw = function() {
-        p5.background(220)
-        if (p5.mouseIsPressed) {
-            p5.noCursor()
-            p5.image(movie, p5.mouseX-60, p5.mouseY-40, 120, 80)
-            movie.loop()
-
-            let rr = parseInt(Math.random() * 255)
-            let rg = parseInt(Math.random() * 255)
-            let rb = parseInt(Math.random() * 255)
-            // console.log(rr, rg, rb)
-
-            // console.log(p5.random(10))
-            p5.fill(p5.color(rr, rg, rb));
-        } else {
-            p5.cursor()
-            p5.fill(255);
+        posX = posX + 1
+        if (posX > 400) {
+            posX = 0
         }
-        // p5.ellipse(p5.mouseX, p5.mouseY, 80, 80);
+        p5.background(80)
+        p5.ellipse(posX, posY,  20, 20);
     }
 }
 new p5(s); // invoke p5, don't invoke 'automagically' !!!!!!
