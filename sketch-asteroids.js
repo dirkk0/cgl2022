fba3bc67b1349b8c3f4464aa3953d59c6fd0a956
Dirk Krause <dirkk0@googlemail.com>
2023-01-11 18:36:40 +0100

    Update sketch-asteroids.js

--- sketch-asteroids.js
+++ sketch-asteroids.js
@@ -3,14 +3,31 @@ const s = (p5) => {
     let posX = 100
     let posY = 100
 
+    let screenX = 600
+    let screenY = 400
+
+    let incX = -3
+    let incY = 2
+
     p5.setup = function() {
-        p5.createCanvas(400, 400);
+        p5.createCanvas(screenX, screenY);
     }
     p5.draw = function() {
-        posX = posX + 1
-        if (posX > 400) {
+        posX = posX + incX
+        posY = posY + incY
+        if (posX > screenX) {
             posX = 0
         }
+        if (posY > screenY) {
+            posY = 0
+        }
+        if (posX <= 0) {
+            posX = screenX
+        }
+        if (posY <= 0) {
+            posY = screenY
+        }
+
         p5.background(80)
         p5.ellipse(posX, posY,  20, 20);
     }
