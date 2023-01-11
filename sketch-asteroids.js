99acee0bb5559bef96e363486c8fb0d291e944d8
Dirk Krause <dirkk0@googlemail.com>
2023-01-11 19:00:43 +0100

    Update sketch-asteroids.js

--- sketch-asteroids.js
+++ sketch-asteroids.js
@@ -1,4 +1,15 @@
+
 const s = (p5) => {
+
+
+    function Thing(x,y) {
+        let posX = x
+        let posY = y
+
+        this.draw = function () {
+            p5.ellipse(posX, posY, 20, 20);
+        }
+    }
 
     let posX = 100
     let posY = 100
@@ -6,30 +17,37 @@ const s = (p5) => {
     let screenX = 600
     let screenY = 400
 
-    let incX = parseInt(Math.random()*10) - 5
-    let incY = parseInt(Math.random()*10) - 5
+    let incX = parseInt(Math.random() * 10) - 5
+    let incY = parseInt(Math.random() * 10) - 5
 
-    p5.setup = function() {
+    let t1 = new Thing(30, 40)
+    let t2 = new Thing(120, 110)
+
+    p5.setup = function () {
         p5.createCanvas(screenX, screenY);
     }
-    p5.draw = function() {
-        posX = posX + incX
-        posY = posY + incY
-        if (posX > screenX) {
-            posX = 0
-        }
-        if (posY > screenY) {
-            posY = 0
-        }
-        if (posX < 0) {
-            posX = screenX
-        }
-        if (posY < 0) {
-            posY = screenY
-        }
-
+    p5.draw = function () {
         p5.background(80)
-        p5.ellipse(posX, posY,  20, 20);
+
+        t1.draw()
+        t2.draw()
+
+        // posX = posX + incX
+        // posY = posY + incY
+        // if (posX > screenX) {
+        //     posX = 0
+        // }
+        // if (posY > screenY) {
+        //     posY = 0
+        // }
+        // if (posX < 0) {
+        //     posX = screenX
+        // }
+        // if (posY < 0) {
+        //     posY = screenY
+        // }
+
+        // p5.ellipse(posX, posY, 20, 20);
     }
 }
 new p5(s); // invoke p5, don't invoke 'automagically' !!!!!!
